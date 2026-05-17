d8417dad233680ef429df4e130f8579ccbf91fc8
Sadia <227906842+ListMist@users.noreply.github.com>
2026-05-17 07:30:17 +0600

    update structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,10 @@
 import { Module } from '@nestjs/common';
-import{TypeOrmModule} from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { User} from './auth/user.entity';
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: 'rankup_db',
-      entities: [User],
-      autoLoadEntities: true,
-      synchronize: true,
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
-    }),
-    AuthModule,
-  ],
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
