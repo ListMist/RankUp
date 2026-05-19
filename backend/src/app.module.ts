93ef94702840dd680d0d2540514f91ce1faed434
Sadia <227906842+ListMist@users.noreply.github.com>
2026-05-20 03:58:05 +0600

    Update Database Connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [],
-  providers: [],
+  imports:[
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Sadia55',
+      database: 'rankUp',
+      autoLoadEntities: true,
+      synchronize: true,
+
+    }),
+  
+   AuthModule,
+  UsersModule
+  ],
+  
 })
 export class AppModule {}
